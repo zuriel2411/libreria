eb070483083e1c7f332a67e49bc1aea5206d5912
javier <zurielcabrera447@gmail.com>
2024-01-30 20:24:13 -0600

    archobos 1

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,7 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("HOLA");
     }
     
 }
