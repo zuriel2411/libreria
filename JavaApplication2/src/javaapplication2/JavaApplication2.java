df782d62aab9f15640651c8c7819d93bad18ef5d
zuriel2411 <zurielcabrera447@gmail.com>
2024-02-06 20:31:42 -0600

    cambio de bariables

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,7 +14,7 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("HOLA");
+        System.out.println("HOcbdfshghfsaghggdgffhgdgfssdfsdgfsdgefwdfgfrefdgrerrerwwfegfDFGHJKHHGFHGGF CTRGFCT YGRTRHEFTGFRTFSYBVCBR G DTSYTG RTHLA");
     }
     
 }
